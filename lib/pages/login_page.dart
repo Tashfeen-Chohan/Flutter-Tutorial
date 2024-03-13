e32433f49c411a79f21bbf3223f081dd991ac776
Tashfeen Chohan <chohantashfeen@gmail.com>
2024-03-14 03:16:46 +0500

    assets & images - TextFormField

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -7,17 +7,50 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Login Page"),
+        title: const Text("Flutter Catalog"),
         backgroundColor: Colors.amberAccent,
       ),
-      body: const Center(
-          child: Text(
-        "Welcome to Login Page",
-        style: TextStyle(
-            fontSize: 30,
-            fontWeight: FontWeight.bold,
-            color: Colors.blueAccent),
-      )),
+      body: Center(
+        child: Column(
+          children: [
+            Image.asset(
+              "assets/images/login_img.jpg",
+              fit: BoxFit.contain,
+              width: 200,
+              height: 200,
+            ),
+            const Text(
+              "Welcome to Login Page!",
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 50),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: const InputDecoration(
+                        hintText: "Please enter username!",
+                        labelText: "Username"),
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                        hintText: "Please enter password",
+                        labelText: "Password"),
+                  ),
+                  const SizedBox(height: 25,),
+                  ElevatedButton(
+                    child: const Text("Login"),
+                    onPressed: () {
+                      print("Hi");
+                    },
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
       drawer: const Drawer(),
     );
   }
